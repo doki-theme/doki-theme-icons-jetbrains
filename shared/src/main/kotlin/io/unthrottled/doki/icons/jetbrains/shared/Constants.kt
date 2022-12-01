3c28dd55c85585c49a7d573cfeb9ef02c453b5a5
Alex Simons <alexsimons9999@gmail.com>
2022-11-30 20:00:45 -0600

    Initial EXP UI support.

--- shared/src/main/kotlin/io/unthrottled/doki/icons/jetbrains/shared/Constants.kt
+++ shared/src/main/kotlin/io/unthrottled/doki/icons/jetbrains/shared/Constants.kt
@@ -4,5 +4,5 @@ object Constants {
   const val REPO_URL = "https://github.com/doki-theme/doki-theme-icons-jetbrains"
   const val PLUGIN_ID = "io.unthrottled.doki.icons"
   const val PLUGIN_NAME = "Doki Theme Icons"
-  const val DOKI_ICONS_BASE_PATH = "/doki/icons"
+  const val DOKI_ICONS_BASE_PATH = "doki/icons"
 }
