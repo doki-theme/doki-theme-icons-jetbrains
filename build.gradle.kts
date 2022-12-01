25fd6a5c68919a4a62428af1fcbcc67d24c6a2d7
Alex Simons <alexsimons9999@gmail.com>
2022-12-01 13:14:16 -0600

    Heavy handed-ly fixed EXP UI...

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,6 +44,7 @@ allprojects {
   }
 
   dependencies {
+    implementation("org.javassist:javassist:3.29.2-GA")
     testImplementation("org.assertj:assertj-core:3.23.1")
     testImplementation("io.mockk:mockk:1.12.7")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
---
+++ shared/src/main/kotlin/io/unthrottled/doki/icons/jetbrains/shared/integrations/PlatformHacker.kt
@@ -0,0 +1,106 @@
+package io.unthrottled.doki.icons.jetbrains.shared.integrations
+
+import com.intellij.ide.ApplicationInitializedListener
+import io.unthrottled.doki.icons.jetbrains.shared.tools.Logging
+import io.unthrottled.doki.icons.jetbrains.shared.tools.logger
+import io.unthrottled.doki.icons.jetbrains.shared.tools.runSafely
+import javassist.ClassClassPath
+import javassist.ClassPool
+import javassist.expr.ExprEditor
+import javassist.expr.MethodCall
+import javassist.expr.NewExpr
+
+object PlatformHacker : ApplicationInitializedListener, Logging {
+
+  init {
+    hackPlatform()
+  }
+
+  fun hackPlatform() {
+    hackEXPUI()
+  }
+
+  private fun hackEXPUI() {
+    fixEXPUIButton()
+    fixEXPUIRunWidget()
+    fixEXPUIStopButton()
+  }
+
+  private fun fixEXPUIStopButton() {
+    runSafely({
+      val cp = ClassPool(true)
+      cp.insertClassPath(
+        ClassClassPath(
+          Class.forName("com.intellij.execution.ui.RunState")
+        )
+      )
+      val ctClass = cp.get("com.intellij.execution.ui.StopWithDropDownAction")
+      val doPaintText = ctClass.getDeclaredMethods("update")[0]
+      doPaintText.instrument(
+        object : ExprEditor() {
+          override fun edit(m: MethodCall?) {
+            if (m?.methodName == "toStrokeIcon") {
+              m.replace("{ \$_ = \$1; }")
+            }
+
+          }
+        }
+      )
+      ctClass.toClass()
+    }) {
+      logger().warn("Unable to hack 'fixEXPUIStopButton' for raisins", it)
+    }
+  }
+
+  private fun fixEXPUIRunWidget() {
+    runSafely({
+      val cp = ClassPool(true)
+      cp.insertClassPath(
+        ClassClassPath(
+          Class.forName("com.intellij.execution.ui.RunState")
+        )
+      )
+      val ctClass = cp.get("com.intellij.execution.ui.RunWidgetButtonLook")
+      val doPaintText = ctClass.getDeclaredMethods("paintIcon")[0]
+      doPaintText.instrument(
+        object : ExprEditor() {
+          override fun edit(m: MethodCall?) {
+            if (m?.methodName == "toStrokeIcon") {
+              m.replace("{ \$_ = \$1; }")
+            }
+
+          }
+        }
+      )
+      ctClass.toClass()
+    }) {
+      logger().warn("Unable to hack 'fixEXPUIRunWidget' for raisins", it)
+    }
+  }
+
+  private fun fixEXPUIButton() {
+    runSafely({
+      val cp = ClassPool(true)
+      cp.insertClassPath(
+        ClassClassPath(
+          Class.forName("com.intellij.openapi.wm.impl.SideStack")
+        )
+      )
+      val ctClass = cp.get("com.intellij.openapi.wm.impl.SquareStripeButtonLook")
+      val doPaintText = ctClass.getDeclaredMethods("paintIcon")[0]
+      doPaintText.instrument(
+        object : ExprEditor() {
+          override fun edit(m: MethodCall?) {
+            if (m?.methodName == "toStrokeIcon") {
+              m.replace("{ \$_ = \$1; }")
+            }
+
+          }
+        }
+      )
+      ctClass.toClass()
+    }) {
+      logger().warn("Unable to hack 'fixEXPUIButton' for raisins", it)
+    }
+  }
+}
